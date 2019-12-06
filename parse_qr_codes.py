768b4984e006027f23b0b60ef9144ce63dc1ed50
Daviji <david.carlip@djusdk12ca.org>
2019-12-05 19:44:24 -0800

    Add QR parsing for scanner (#1)
    Parse data from QR scanner and upload it to MongoDB.
    Data is given as user input using USB keyboard emulation.
    QR Scanner 'Endmarke' setting must be set to tabs.

---
+++ parse_qr_codes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.6
+"""Parse data from QR scanner and upload it to MongoDB.
+
+Data is given as user input using USB keyboard emulation.
+QR Scanner 'Endmarke' setting must be set to tabs"""
+
+# '\t' refers to tab character, rstrip removes trailing tab from Endmark of last QR code
+QR_DATA = input("Scan data here: ").rstrip('\t').split('\t')
+
+# TODO: Upload data to MongoDB
