09eee9a939cf0ebe5fe645d60f7d489a52059ab4
Kevin Rockwell <55220125+kevinrockwell@users.noreply.github.com>
2020-01-02 19:18:37 -0800

    Format double quoted strings as single quoted (#13)
    Update for consistency with rest of project

--- parse_qr_codes.py
+++ parse_qr_codes.py
@@ -5,6 +5,6 @@ Data is given as user input using USB keyboard emulation.
 QR Scanner 'Endmarke' setting must be set to tabs"""
 
 # '\t' refers to tab character, rstrip removes trailing tab from Endmark of last QR code
-QR_DATA = input("Scan data here: ").rstrip('\t').split('\t')
+QR_DATA = input('Scan data here: ').rstrip('\t').split('\t')
 
 # TODO: Upload data to MongoDB
