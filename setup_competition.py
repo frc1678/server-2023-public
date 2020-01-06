d8d44dde801a5091fca54615be1d76edf13b9e26
loganhaug <46719945+LoganHaug@users.noreply.github.com>
2020-01-06 11:27:14 -0800

    Fix style & types in setup_competition.py (#19)
    - Fix style to pass PEP8
    - Change dicts in competition document to lists, namely in 'raw', and in 'calculated', initially should have been lists, and because it needs to store dictionaries of lists

--- setup_competition.py
+++ setup_competition.py
@@ -4,45 +4,46 @@
 Run before every competition.
 """
 # External imports
-from pymongo import MongoClient
 import re
+from pymongo import MongoClient
 # Internal imports
 import utils
 
-# Makes a connection with the local database through port 27017, the default listening port of MongoDB
-db = MongoClient('localhost', 27017).scouting_system
+# Makes connection with local database through port 27017, the default listening port of MongoDB
+DB = MongoClient('localhost', 27017).scouting_system
 
-competition_code = input('Input the competition code from TBA: ')
+COMPETITION_CODE = input('Input the competition code from TBA: ')
 # Use a regular expression to determine if competition code is in the correct format
 # First capture group: Matches 4 digits
 # Second capture group: Matches 1 or more letters
-code_match = re.fullmatch(r'(?P<year>[0-9]{4})(?P<comp_code>[a-zA-Z]+)', competition_code)
-if code_match is None:
+CODE_MATCH = re.fullmatch(r'(?P<year>[0-9]{4})(?P<comp_code>[a-zA-Z]+)', COMPETITION_CODE)
+if CODE_MATCH is None:
     raise Exception('Competition code is not in the correct format')
-# Creates the competition.txt file, and writes the competition code to it so it can be used in other scripts.
+# Creates the competition.txt file, writes the competition code to it so it can be used in other
+# scripts.
 with open(utils.create_file_path('data/competition.txt'), 'w') as file:
-    file.write(competition_code)
+    file.write(COMPETITION_CODE)
 
 # Checks that the competition inputted by the user is not already in the database
-if db.competitions.count_documents({'tba_event_code': competition_code}) != 0:
-    raise Exception(f'The competition {competition_code} already exists in the database.')
+if DB.competitions.count_documents({'tba_event_code': COMPETITION_CODE}) != 0:
+    raise Exception(f'The competition {COMPETITION_CODE} already exists in the database.')
 
 # Extracts the year with capture group
-year = int(code_match.group('year'))
+YEAR = int(CODE_MATCH.group('year'))
 # Inserts document into collection
-db.competitions.insert_one({
-    'year': year,
-    'tba_event_code': competition_code,
+DB.competitions.insert_one({
+    'year': YEAR,
+    'tba_event_code': COMPETITION_CODE,
     'raw': {
-        'qr_obj': {},
-        'qr_subj': {},
-        'pit': {},
+        'qr_obj': [],
+        'qr_subj': [],
+        'pit': [],
     },
     'calculated': {
-        'unconsolidated_obj_tim': {},
-        'consolidated_obj_tim': {},
-        'subj_aim': {},
-        'team': {},
-        'match': {}, 
+        'unconsolidated_obj_tim': [],
+        'consolidated_obj_tim': [],
+        'subj_aim': [],
+        'team': [],
+        'match': [],
     },
 })
