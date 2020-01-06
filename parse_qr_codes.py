4f5d558a32f58a59662a7fb8d391289ae7a6a62e
Daviji <david.carlip@djusdk12ca.org>
2020-01-06 11:04:04 -0800

    Fix spelling of 'end mark' in second occurrence (#17)
    Endmarke is the German spelling used in the manual, this is a natural
    English usage so it should be end mark.

--- parse_qr_codes.py
+++ parse_qr_codes.py
@@ -4,7 +4,7 @@
 Data is given as user input using USB keyboard emulation.
 QR Scanner 'Endmarke' setting must be set to tabs"""
 
-# '\t' refers to tab character, rstrip removes trailing tab from Endmark of last QR code
+# '\t' refers to tab character, rstrip removes trailing tab from end mark of last QR code
 QR_DATA = input('Scan data here: ').rstrip('\t').split('\t')
 
 # TODO: Upload data to MongoDB
