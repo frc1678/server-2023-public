018f8298ac155bf1bd91fe0fdd861cde87bc5319
JakeRoggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2020-01-11 15:00:25 -0800

    Add base architecture for server.py (#49)

---
+++ server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3.6
+"""Main server file. Runs computations in infinite loop.
+
+Runs on an Ubuntu 18.04 LTS computer in the stands at
+competition. This script runs all of the computations in the server.
+"""
+# No external imports
+# No internal imports
+
+while True:
+    # TODO: Objective TIM decompression
+
+    # TODO: Pull match results from TBA
+
+    # TODO: Consolidation
+
+    # TODO: TIM calcs
+
+    # TODO: Team calcs
+
+    # TODO: Pull rankings from TBA
+
+    # TODO: Match calcs
+
+    # TODO: Sub aim decompression
+
+    # TODO: Team calcs (driver ability)
+
+    pass
