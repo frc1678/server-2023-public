6437e79e595a1eaca691ba59a38e89d9acf612e6
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2020-01-11 15:21:37 -0800

    Update parse_qr_codes.py (#58)
    - Update comments & docstrings
    - Rename variables, functions, and files to be more intuitive + follow server style
    - Simplify if/else statements
    - Remove unnecessary optional arguments
    - Modify error message to provide more information

--- input_qr_codes.py
+++ input_qr_codes.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3.6
-"""Takes user input for QR codes over QR scanner, and runs parse_qr_codes()
+"""Takes user input for QR codes from QR scanner and sends them to local database
 
-Takes user input for QR codes
-Removes end tab from QR scanner, and splits user input by tabs for parse_qr_codes.py
-QR Scanner 'Endmarke' setting must be set to tabs
+QR scanner 'Endmarke' setting must be set to tabs
 """
 # No external imports
 # Internal imports
-import parse_qr_codes
+import qr_code_uploader
 
-# Takes user input for the QR data
+# Takes user input from QR scanner
 QR_DATA = input('Scan data here: ')
 
 # If the user input is nothing, nothing gets added to the database
 if QR_DATA == '':
     raise ValueError('There is no data entered')
 
-# '\t' refers to tab character, rstrip removes trailing tab from end mark of last QR code
+# '\t' refers to tab character; rstrip removes trailing tab from end mark of last QR code
 QR_DATA = QR_DATA.rstrip('\t').split('\t')
 
-# Parses the QR codes, see parse_qr_codes.py for more info
-parse_qr_codes.parse_qr_codes(QR_DATA)
+# Uploads QR codes to database
+qr_code_uploader.upload_qr_codes(QR_DATA)
--- qr_code_uploader.py
+++ qr_code_uploader.py
@@ -12,21 +12,20 @@ from pymongo import MongoClient
 # Internal imports
 from utils import create_file_path
 
-def parse_qr_codes(qr_data):
-    """Parses QR codes and inserts them into the competition document.
+def upload_qr_codes(qr_codes):
+    """Uploads QR codes into the current competition document.
 
-    Splits QR codes into objective and subjective.
-    Checks for duplicates within set of QR codes to add, and the set of QR codes to add and the
-    database.
-    Appends new QR codes to raw.obj and raw.subj
+    Adds each QR code into its appropriate dataset.
+    Prevents duplicate QR codes from being uploaded to the database.
+
+    qr_codes is a list of QR code strings to upload
     """
 
-    # Reads the schema file so that it can be accessed as a dictionary later
-    with open(create_file_path('schema/match_collection_qr_schema.yml', False)) as schema_file:
+    # Gets the starting character for each QR code type, used to identify QR code type
+    with open(create_file_path('schema/match_collection_qr_schema.yml')) as schema_file:
         schema = yaml.load(schema_file, yaml.Loader)
 
-    # Reads competition.txt file, which only contains the competition code, in order to find the
-    # competition document
+    # Gets TBA event code of current competition
     with open('data/competition.txt') as file:
         competition_code = file.read()
 
@@ -38,23 +37,21 @@ def parse_qr_codes(qr_data):
     qr_obj = []
     qr_subj = []
 
-    # Appends individual QR codes to QR_CODES
-    # Checks the schema document for the starting character of subjective and objective QR codes,
-    # and splits the QR codes into objective and subjective QR codes at the same time, if the QR
-    # code isn't already the already split list, or already in the database it doesn't add it
-    for qr_code in qr_data:
+    for qr_code in qr_codes:
+        # Adds QR code to its dataset if it hasn't already been added
+        # Identifies QR code type based on its first character
         if qr_code.startswith(schema['subjective']['_start_character']):
-            if qr_code not in qr_subj and qr_code not in raw_data['qr_subj']:
+            if qr_code in qr_subj or qr_code in raw_data['qr_subj']:
+                print(f'Duplicate QR code not uploaded: "{qr_code}"')
+            else:
                 qr_subj.append(qr_code)
-            else:
-                print(f'Removed a duplicate subjective QR code: {qr_code}')
         elif qr_code.startswith(schema['objective_tim']['_start_character']):
-            if qr_code not in qr_obj and qr_code not in raw_data['qr_obj']:
-                qr_obj.append(qr_code)
+            if qr_code in qr_obj or qr_code in raw_data['qr_obj']:
+                print(f'Duplicate QR code not uploaded: "{qr_code}"')
             else:
-                print(f'Removed a duplicate objective QR code: {qr_code}')
+                qr_obj.append(qr_code)
         else:
-            raise ValueError('QR Code does not have a valid start character')
+            raise ValueError(f'Invalid QR code - no start character: "{qr_code}"')
 
     # Adds the objective and subjective QR codes to the local database if the lists aren't empty
     if qr_obj != []:
