921642ff4832436b97eae3ed26045b7c50aa46ec
Thomas Doherty <55568221+ThomasDoherty5@users.noreply.github.com>
2020-02-01 11:20:26 -0800

    Add average function (#135)
    - Add average function to utils.py for calculations

--- utils.py
+++ utils.py
@@ -112,6 +112,26 @@ def run_command(command, return_output=False):
     return None
 
 
+def avg(nums, weights=None, default=0):
+    """Calculates the average of a list of numeric types
+
+    If the optional parameter weights is given, calculates a weighted average
+    weights should be a list of floats
+    The length of weights must be the same as the length of nums
+    default is the value returned if nums is an empty list
+    """
+    if len(nums) == 0:
+        return default
+    if weights is None:
+        # Normal (not weighted) average
+        return sum(nums) / len(nums)
+    # Expect one weight for each number
+    if len(nums) != len(weights):
+        raise ValueError(f'Weighted average expects one weight for each number.')
+    weighted_sum = sum([num * weight for num, weight in zip(nums, weights)])
+    return weighted_sum / sum(weights)
+
+
 # Specifies which event - string such as '2020cada'.
 with open(create_file_path(_TBA_KEY_FILE)) as file:
     # Remove trailing newline (if it exists) from file data.
