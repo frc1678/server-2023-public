3c8d018685dd218b5031d0c09f6cb7f9b0391291
Kevin Rockwell <55220125+kevinrockwell@users.noreply.github.com>
2020-02-17 09:30:18 -0800

    Fix local_database_communicator merge issues (#229)
    Adds implementation of QR data pulling back into server.py

--- server.py
+++ server.py
@@ -5,8 +5,12 @@
 Runs on an Ubuntu 18.04 LTS computer in the stands at
 competition. This script runs all of the computations in the server.
 """
-# No external imports
+# External imports
+import json
+import os
+import re
 # Internal imports
+import adb_communicator
 import calculate_tims
 import decompressor
 import cloud_database_communicator
@@ -38,6 +42,8 @@ def get_empty_modified_data():
 # Tracks if cloud database has been updated since the latest server restart
 # Is used to determine if raw.qr should be completely emptied or just pushed to
 SERVER_RESTART_SINCE_CLOUD_UPDATE = True
+# Tracks if this is the first calculation cycle since server restart
+SERVER_RESTART = True
 # Instantiate the queue that stores what changes need to be pushed to cloud
 CLOUD_DB_QUEUE = get_empty_modified_data()
 # Instantiate the queue that tracks what data has been changed
@@ -52,13 +58,55 @@ MAIN_QUEUE['raw']['qr'] = [qr for qr in MAIN_QUEUE['raw']['qr'] if qr not in BLA
 # Where we get the match data from TBA
 MATCH_LIST = []
 
+# Open the tablet serials file to find all device serials
+with open('data/tablet_serials.json') as serial_numbers:
+    DEVICE_SERIAL_NUMBERS = json.load(serial_numbers)
+
 while True:
+    # Parses 'adb devices' to find num of devices so that don't try to pull from nothing
+    if len(utils.run_command('adb devices', True).split('\n')) != 1:
+        TABLET_QR_DATA, DEVICE_FILE_PATHS = [], []
+        DEVICE_FILE_PATH = 'data/tablets'
+        # Pull all files from the 'Download' folder on the tablet
+        adb_communicator.adb_pull_tablet_data(DEVICE_FILE_PATH, '/storage/emulated/0/Download')
+        # Iterates through the 'data' folder
+        for device_dir in os.listdir(DEVICE_FILE_PATH):
+            if device_dir in DEVICE_SERIAL_NUMBERS.keys():
+                DEVICE_FILE_PATHS.append(device_dir)
+            # If the folder name is a device serial, it must be a tablet folder
+        for device in DEVICE_FILE_PATHS:
+            # Iterate through the downloads folder in the device folder
+            for file in os.listdir(f'{DEVICE_FILE_PATH}/{device}/Download'):
+                # Makes a regex pattern that matches the file name of a QR file
+                # Format of obj QR file pattern: <qual_num>_<team_num>_<serial_num>_<timestamp>.txt
+                obj_pattern = re.compile(r'[0-9]+_[0-9]+_[A-Z0-9]+_[0-9]+.txt')
+                # Format of subj QR file pattern: <qual_num>_<serial_num_<timestamp>.txt
+                subj_pattern = re.compile(r'[0-9]+_[0-9A-Z]+_[0-9]+.txt')
+                if re.fullmatch(obj_pattern, file) is not None or \
+                        re.fullmatch(subj_pattern, file) is not None:
+                    with open(f'{DEVICE_FILE_PATH}/{device}/Download/{file}') as qr_code_file:
+                        qr_code = qr_code_file.read()
+                        # If the QR code has a newline at the end, remove it
+                        qr_code = qr_code.rstrip('\n')
+                        # If the QR code isn't blacklisted or already in MAIN_QUEUE add it
+                        if qr_code not in BLACKLISTED and qr_code not in MAIN_QUEUE:
+                            TABLET_QR_DATA.append(qr_code)
+        # Add the QR codes to MAIN_QUEUE and upload them
+        MAIN_QUEUE['raw']['qr'].extend(qr_code_uploader.upload_qr_codes(TABLET_QR_DATA))
     RAW_SCANNER = input('Scan Data Here: ')
     # Do not try to upload blank string
     if RAW_SCANNER != '':
         QR_DATA = RAW_SCANNER.rstrip('\t').split('\t')
         # Upload QRs to MongoDB and add uploaded QRs to queue
         MAIN_QUEUE['raw']['qr'].extend(qr_code_uploader.upload_qr_codes(QR_DATA))
+
+    # Empty decompressed QRs in the database if this is the first cycle since server restart
+    # Otherwise, decompressed QRs will potentially stack as server restarts could entail schema
+    # or decompression code changes, and selection purely by team/match number would not work as
+    # there are multiple QRs for a match
+    if SERVER_RESTART:
+        local_database_communicator.remove_data('processed.unconsolidated_obj_tim')
+        local_database_communicator.remove_data('processed.subj_aim')
 
     # Decompress all inputted QRs
     DECOMPRESSED_QRS = decompressor.decompress_qrs(MAIN_QUEUE['raw']['qr'])
@@ -69,8 +117,17 @@ while True:
     local_database_communicator.append_or_overwrite(
         'processed.subj_aim', DECOMPRESSED_QRS['subj_aim'])
     # Add decompressed QRs to queues
+    # Iterate through both objective and subjective QRs
     for qr_type in DECOMPRESSED_QRS:
-        MAIN_QUEUE['processed'][qr_type].extend(DECOMPRESSED_QRS[qr_type])
+        for qr in DECOMPRESSED_QRS[qr_type]:
+            # Include match number in information to be added to queue
+            QR_ID = {'match_number': qr['match_number']}
+            # Only include team number for objective QRs
+            if qr_type == 'unconsolidated_obj_tim':
+                QR_ID['team_number'] = qr['team_number']
+            # Only add QR if it is not already in queue
+            if QR_ID not in MAIN_QUEUE['processed'][qr_type]:
+                MAIN_QUEUE['processed'][qr_type].append(QR_ID)
 
     # TBA_MATCH_DATA is a list of dictionaries
     TBA_MATCH_DATA = tba_communicator.tba_request(f'event/{utils.TBA_EVENT_KEY}/matches')
@@ -116,3 +173,6 @@ while True:
         CLOUD_DB_QUEUE = get_empty_modified_data()
         if SERVER_RESTART_SINCE_CLOUD_UPDATE:
             SERVER_RESTART_SINCE_CLOUD_UPDATE = False
+
+    if SERVER_RESTART:
+        SERVER_RESTART = False
