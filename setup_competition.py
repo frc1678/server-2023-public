66add1a9d4bf37eef06907e1d904706092f656fb
Kevin Rockwell <55220125+kevinrockwell@users.noreply.github.com>
2020-02-17 16:02:20 -0800

    Formatting and minor improvements to setup_competition.py (#226)
    * Remove YEAR from setup_competition.py
    
    - Was unused
    
    * Use `list` built-in instead of list comprehension
    
    - Use of list comprehension is not needed as it was just to turn the result into a list
    
    * Fix docstring formatting in setup_competition.py
    
    * Rearrange newlines
    
    - Existing newline separated comment from the code it addressed

--- setup_competition.py
+++ setup_competition.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) 2019 FRC Team 1678: Citrus Circuits
-"""Sets up the MongoDB document for a competition.
-Run before every competition.
-"""
+"""Sets up the MongoDB document for a competition, should be run before every competition."""
 # External imports
 import re
 from pymongo import MongoClient
@@ -19,18 +17,16 @@ COMPETITION_KEY = input('Input the competition code from TBA: ')
 # Use a regular expression to determine if competition code is in the correct format
 # First capture group: Matches 4 digits
 # Second capture group: Matches 1 or more letters
-
 CODE_MATCH = re.fullmatch(r'(?P<year>[0-9]{4})(?P<comp_code>.+)', COMPETITION_KEY)
 if CODE_MATCH is None:
     raise ValueError('Competition code is not in the correct format')
+
 # Creates the competition.txt file.
 # Also writes the competition code to it so it can be used in other scripts.
 utils.save_event_key(COMPETITION_KEY)
 # Checks that the competition inputted by the user is not already in the database
-if len([comp for comp in DB.competitions.find({'tba_event_key': COMPETITION_KEY})]) != 0:
+if len(list(DB.competitions.find({'tba_event_key': COMPETITION_KEY}))) != 0:
     raise Exception(f'The competition {COMPETITION_KEY} already exists in the database.')
-# Extracts the year with capture group
-YEAR = int(CODE_MATCH.group('year'))
 # Inserts document into collection
 local_database_communicator.add_competition(local_database_communicator.DB, COMPETITION_KEY)
 cloud_database_communicator.add_competition_cloud(COMPETITION_KEY)
