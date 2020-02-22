8f41a274af6cad33e1a14fdf911bc7fe77f13667
JakeRoggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2020-02-22 11:36:32 -0800

    Remove parameter and hard code 1.30 font size (#262)

--- clean_tablets.py
+++ clean_tablets.py
@@ -4,24 +4,19 @@
 # External imports
 import time
 # Internal imports
+import adb_communicator
 import utils
 
-def adb_font_size_enforcer(font_size):
-    # Stores output from 'adb devices'
-    # 'adb devices' returns the serial numbers of all devices connected over ADB.
-    # Example output of 'adb devices':
-    # "List of devices attached\nHA0XUZA9\tdevice\n9AMAY1E53P\tdevice"
-    output = utils.run_command('adb devices', return_output=True)
-    # [1:] removes 'List of devices attached'
-    output = output.rstrip('\n').split('\n')[1:]
-    # Remove '\tdevice' from each line
-    devices = [line.split('\t')[0] for line in output]
+def adb_font_size_enforcer():
+    """Enforce tablet font size to 1.30, the largest supported size"""
+    devices = adb_communicator.get_attached_devices()
     # Wait for USB connection to initialize
     time.sleep(.1)
     for device in devices:
         # The -s flag specifies the device by its serial number.
         utils.run_command(
-            f'adb -s {device} shell settings put system font_scale {font_size}',
+            f'adb -s {device} shell settings put system font_scale 1.30',
             return_output=False
         )
 
+adb_font_size_enforcer()
