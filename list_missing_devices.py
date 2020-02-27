f45a7fde3aebda18fa03e8521467fd0ee4c3754e
JakeRoggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2020-02-26 20:24:37 -0800

    Add device list norm for output of devices (#299)
    Add clarifying messages in missing device list

--- list_missing_devices.py
+++ list_missing_devices.py
@@ -9,6 +9,8 @@ import adb_communicator
 def missing_devices():
     """Loops through device serials and checks if the device is connected then prints in color 
     based on missing status."""
+    print('\033[93m' + 'Normally Pixel 3a #1-3 and Lenovo Tab E7 #1-28 should be here.')
+    print('Lenovo Tab E7 #29-33 are not included in the tablet cases unless tablets have been switched out.')
     devices = adb_communicator.get_attached_devices()
     # Gets all devices
     for device in adb_communicator.DEVICE_SERIAL_NUMBERS:
