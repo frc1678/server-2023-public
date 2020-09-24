1a17006129f358731e9646811b70419c543d73d8
JakeRoggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2020-09-23 20:50:18 -0700

    Have list_missing_devices use termcolor library (#354)
    - More cross platform than using raw ANSI escape codes

--- list_missing_devices.py
+++ list_missing_devices.py
@@ -2,23 +2,25 @@
 # Copyright (c) 2019 FRC Team 1678: Citrus Circuits
 """Elegantly displays attached and missing devices."""
 
+from termcolor import colored
+
 import adb_communicator
 
 def missing_devices():
-    """Loops through device serials and checks if the device is connected then prints in color 
+    """Loops through device serials and checks if the device is connected
 
-    based on missing status.
+    prints in color based on missing status.
     """
-    print('\033[93m' + 'Normally Pixel 3a #1-3 and Lenovo Tab E7 #1-28 should be here.')
+    print(colored('Normally Pixel 3a #1-3 and Lenovo Tab E7 #1-28 should be here.', 'yellow'))
     print('Lenovo Tab E7 #29-33 are not included in the tablet cases unless tablets have been switched out.')
     devices = adb_communicator.get_attached_devices()
     # Gets all devices
     for device in adb_communicator.DEVICE_SERIAL_NUMBERS:
         # Checks if device is connected
         if device not in devices:
-            print('\033[91m' + adb_communicator.DEVICE_SERIAL_NUMBERS[device]) # Red
+            print(colored(adb_communicator.DEVICE_SERIAL_NUMBERS[device], 'red'))
         else:
-            print('\033[92m' + adb_communicator.DEVICE_SERIAL_NUMBERS[device]) # Green
+            print(colored(adb_communicator.DEVICE_SERIAL_NUMBERS[device], 'green'))
 
 
 missing_devices()
