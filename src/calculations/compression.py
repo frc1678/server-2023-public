f06b556fdd397c370b0227b816eca5755213fd13
Kevin Rockwell <55220125+kevinrockwell@users.noreply.github.com>
2020-11-05 19:39:46 -0800

    Add QR Compression (#400)
    * Add compression using code from the modify data PR
    
    * Add tests for compress timeline & compress generic
    
    * Add compress_section to compression & use in obj
    
    * Move compress_list
    
    * Test full objective tim compression
    
    * Fix compress_subj_aim to reflect compress_list changes
    
    * Add test for full subjective compression
    
    * Use compress_section in compress_subj_aim
    
    * Remove unused compress_generic_data and tests
    
    * Add docstring, shebang, and copyright
    
    * Fix typo in docstring
    
    * Remove undeeded import
    
    * Add test_compress_list
    
    * Make quotes single quotes

---
+++ src/calculations/compression.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020 FRC Team 1678: Citrus Circuits
+"""Implements server side compressed
+
+This is used to allow QR codes to be modified and fake data to be generated for system tests.
+"""
+
+from calculations import decompressor
+import utils
+
+
+SCHEMA = utils.read_schema('schema/match_collection_qr_schema.yml')
+
+
+def compress_timeline(timeline_data):
+    """Compresses timeline into string representation"""
+    compressed_actions = []
+    for entry in timeline_data:
+        action_components = []
+        for field in decompressor._TIMELINE_FIELDS:
+            if field['name'] == 'action_type':
+                # Add compressed action representation to action components
+                action_components.append(SCHEMA['action_type'][entry['action_type']])
+            else:
+                value = str(entry[field['name']])
+                # Pad left of value with zeroes to length defined in schema
+                value = value.rjust(field['length'], '0')
+                action_components.append(value)
+        compressed_actions.extend(action_components)
+    return ''.join(compressed_actions)
+
+
+def compress_list(section, data):
+    """Compresses a list given the section of schema"""
+    string_data = (str(x) for x in data)
+    return SCHEMA[section]['_separator_internal'].join(string_data)
+
+
+def compress_section(data, section):
+    """Compress any section from the QR schema"""
+    compressed_points = []
+    # Schema version is required to be at the start and exists for every version
+    if section == 'generic_data' and 'schema_version' not in data:
+        compressed_points.append(f'A{SCHEMA["schema_file"]["version"]}')
+    # TODO Handle bools (not needed currently because no bools)
+    for key, value in data.items():
+        compressed_key = SCHEMA[section][key][0]
+        if key == 'timeline':
+            compressed_value = compressed_key + compress_timeline(value)
+        elif SCHEMA[section][key][1] == 'list':
+            compressed_value = compressed_key + compress_list(section, value)
+        else:
+            compressed_value = f'{compressed_key}{str(value).upper()}'
+        if key == 'schema_version':
+            compressed_points.insert(0, compressed_value)
+        else:
+            compressed_points.append(compressed_value)
+    return SCHEMA[section]['_separator'].join(compressed_points)
+
+
+def compress_obj_tim(tim_data):
+    """Compresses a tim into QR string representation."""
+    generic_points = {}
+    obj_points = {}
+    for name, data in tim_data.items():
+        if name in SCHEMA['generic_data']:
+            generic_points.update({name: data})
+        elif name in SCHEMA['objective_tim']:
+            obj_points.update({name: data})
+    # Compress Generic Data
+    compressed_generic = compress_section(generic_points, 'generic_data')
+    # Compress Objective Data
+    compressed_obj = compress_section(obj_points, 'objective_tim')
+    compressed_qr = SCHEMA['generic_data']['_section_separator'].join(
+        [compressed_generic, compressed_obj]
+    )
+    return f'{SCHEMA["objective_tim"]["_start_character"]}{compressed_qr}'
+
+
+def compress_subj_aim(aim_data):
+    """Compresses subjective QRs"""
+    generic_points = {}
+    subj_points = {}
+    for name, data in aim_data.items():
+        if name in SCHEMA['generic_data']:
+            generic_points.update({name: data})
+        elif name in SCHEMA['subjective_aim']:
+            subj_points.update({name: data})
+    # Compress Generic Data
+    compressed_generic = compress_section(generic_points, 'generic_data')
+    compressed_subj = compress_section(subj_points, 'subjective_aim')
+    compressed_qr = SCHEMA['generic_data']['_section_separator'].join(
+        [compressed_generic, compressed_subj]
+    )
+    return f'{SCHEMA["subjective_aim"]["_start_character"]}{compressed_qr}'
---
+++ tests/calculations/test_compression.py
@@ -0,0 +1,81 @@
+from calculations import compression
+
+
+def test_compress_timeline():
+    timeline_data = [
+        {'time': 51, 'action_type': 'start_incap'},
+        {'time': 32, 'action_type': 'end_incap'},
+    ]
+    assert compression.compress_timeline(timeline_data) == '051AC032AD'
+    timeline_data[1]['action_type'] = 'score_ball_high'
+    assert compression.compress_timeline(timeline_data) == '051AC032AA'
+
+
+def test_compress_list():
+    test_list = [1, 2, 3]
+    assert compression.compress_list('subjective_aim', test_list) == '1:2:3'
+
+
+def test_compress_section_generic_data():
+    # Make sure it adds schema version
+    assert compression.compress_section({}, 'generic_data') == 'A18'
+    # Check generic data compression
+    schema_data = {'schema_version': 18}
+    compressed_schema = 'A18'
+    assert compression.compress_section(schema_data, 'generic_data') == compressed_schema
+    # Check multiple points
+    schema_data['serial_number'] = 'test'
+    # Qrs are only uppercase
+    compressed_schema += '$BTEST'
+    assert compression.compress_section(schema_data, 'generic_data') == compressed_schema
+
+
+def test_compress_section_obj():
+    # Without timeline
+    schema_data = {'team_number': 1678, 'scout_id': 18}
+    compressed_schema = 'Z1678$X18'
+    assert compression.compress_section(schema_data, 'objective_tim') == compressed_schema
+    # With timeline
+    schema_data['timeline'] = [{'time': 51, 'action_type': 'start_incap'}]
+    compressed_schema += '$W051AC'
+    assert compression.compress_section(schema_data, 'objective_tim') == compressed_schema
+
+
+def test_compress_section_subj():
+    data = {'rendezvous_agility_rankings': [1, 2, 3], 'agility_rankings': [2, 3, 1]}
+    assert compression.compress_section(data, 'subjective_aim') == 'A1:2:3$B2:3:1'
+
+
+def test_compress_obj_tim():
+    data = {
+        'schema_version': 18,
+        'serial_number': 'HASAMPLENUM',
+        'match_number': 1,
+        'timestamp': 1582994470,
+        'match_collection_version_number': '1.0.2',
+        'team_number': 9999,
+        'scout_name': 'KEVIN R',
+        'scout_id': 2,
+        'timeline': [
+            {'time': 45, 'action_type': 'start_incap'},
+            {'time': 7, 'action_type': 'end_incap'},
+        ],
+    }
+    compressed_data = '+A18$BHASAMPLENUM$C1$D1582994470$E1.0.2$FKEVIN R%Z9999$X2$W045AC007AD'
+    assert compression.compress_obj_tim(data) == compressed_data
+
+
+def test_compress_subj_aim():
+    data = {
+        'schema_version': 18,
+        'serial_number': 'HASAMPLENUM',
+        'match_number': 1,
+        'timestamp': 1582994470,
+        'match_collection_version_number': '1.0.2',
+        'team_number': 9999,
+        'scout_name': 'KEVIN R',
+        'rendezvous_agility_rankings': [1, 2, 3],
+        'agility_rankings': [2, 3, 1],
+    }
+    compressed_data = '*A18$BHASAMPLENUM$C1$D1582994470$E1.0.2$FKEVIN R%A1:2:3$B2:3:1'
+    assert compression.compress_subj_aim(data) == compressed_data
