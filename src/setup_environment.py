4a95cdd7922de5d7175fd87fb1bb3ba1c4bcafcf
JakeRoggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2021-01-24 11:30:21 -0800

    Add logger to setup_environment to check for error (#449)
    Allows script output to be cleaner while still allowing error messages to be seen if needed

--- src/setup_environment.py
+++ src/setup_environment.py
@@ -11,6 +11,36 @@ import venv
 
 import utils
 
+
+class Logger:
+    def __enter__(self):
+        """Creates file and returns self"""
+        self.name = 'setup_environment.log'
+        self.path = utils.create_file_path(self.name)
+        self.log_file = open(self.path, 'w')
+        self.should_destroy_log = False
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """Checks if the discard method has been called
+
+        i.e. if it ran successfully and if we should discard the log"""
+        if self.should_destroy_log:
+            os.remove(self.path)
+        else:
+            print('\n\nSomething broke! Check setup_environment.log for details')
+
+    def log(self, message: str, should_output: bool = True):
+        """Adds something to the file, prints it by default"""
+        self.log_file.write(f'\n{message}')
+        if should_output:
+            print(message)
+
+    def discard(self):
+        """Changes the state of the file being destroyed"""
+        self.should_destroy_log = True
+
+
 # Creates data folder
 utils.create_file_path('data/')
 
@@ -19,29 +49,41 @@ TARGET_PATH = utils.create_file_path('.venv', False)
 
 # Only run if file is directly called
 if __name__ == '__main__':
-    # Create virtual environment
-    print('Creating Virtual Environment...')
-    # Clear any existing environments with clear=True
-    # Install pip directly into installation
-    # Set prompt to 'venv' instead of '.venv'
-    venv.create(TARGET_PATH, clear=True, with_pip=True, prompt='venv')
-    print('Virtual Environment Created')
+    with Logger() as LOGGER:
+        log = LOGGER.log
+        # Create virtual environment
+        log('Creating Virtual Environment...')
+        # Clear any existing environments with clear=True
+        # Install pip directly into installation
+        # Set prompt to 'venv' instead of '.venv'
+        venv.create(TARGET_PATH, clear=True, with_pip=True, prompt='venv')
+        log('Virtual Environment Created')
 
-    # Install pip packages
-    # Set create_directories to false to avoid trying to create directory for pip
-    if os.name == 'nt':
-        # Windows uses a Scripts directory instead of a bin directory
-        PIP_PATH = utils.create_file_path('.venv/Scripts/pip', False)
-    else:
-        PIP_PATH = utils.create_file_path('.venv/bin/pip', False)
-    REQUIREMENTS_PATH = utils.create_file_path('requirements.txt')
-    print('Installing PyPI Packages...')
-    # Runs /path/to/pip install -r /path/to/requirements.txt
-    # Sets check to True to force check that pip exited without errors
-    # Pipes pip output to /dev/null to avoid cluttering the terminal
-    # All pip warnings/errors still print to terminal because stderr is not redirected
-    subprocess.run([PIP_PATH, 'install', 'wheel'], check=True, stdout=subprocess.DEVNULL)
-    subprocess.run(
-        [PIP_PATH, 'install', '-r', REQUIREMENTS_PATH], check=True, stdout=subprocess.DEVNULL
-    )
-    print('Environment Setup Complete')
+        # Install pip packages
+        # Set create_directories to false to avoid trying to create directory for pip
+        if os.name == 'nt':
+            # Windows uses a Scripts directory instead of a bin directory
+            PIP_PATH = utils.create_file_path('.venv/Scripts/pip', False)
+        else:
+            PIP_PATH = utils.create_file_path('.venv/bin/pip', False)
+        REQUIREMENTS_PATH = utils.create_file_path('requirements.txt')
+        log('Installing PyPI Packages...')
+        # Runs /path/to/pip install -r /path/to/requirements.txt
+        # Sets check to True to force check that pip exited without errors
+        # Pipes pip output to /dev/null to avoid cluttering the terminal
+        # All pip warnings/errors still print to terminal because stderr is not redirected
+        install_wheel = subprocess.run(
+            [PIP_PATH, 'install', 'wheel'],
+            check=True,
+            stdout=LOGGER.log_file,
+            stderr=LOGGER.log_file,
+        )
+        install_reqs = subprocess.run(
+            [PIP_PATH, 'install', '-r', REQUIREMENTS_PATH],
+            check=True,
+            stdout=LOGGER.log_file,
+            stderr=LOGGER.log_file,
+        )
+        log('Environment Setup Complete')
+        if install_wheel.returncode == 0 and install_reqs.returncode == 0:
+            LOGGER.discard()
