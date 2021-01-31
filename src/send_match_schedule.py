ae6676820b39307611e97345e26b688444c53fb6
JakeRoggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2021-01-31 11:05:42 -0800

    Refactor send_match_schedule to make it importable (#462)

--- src/send_match_schedule.py
+++ src/send_match_schedule.py
@@ -7,33 +7,53 @@ scout tablets over ADB, and verify that the file is successfully transferred. AD
 Android Debug Bridge.
 """
 
+import argparse
 import csv
 import hashlib
-import json
 import time
+from typing import List
+from dataclasses import dataclass
 
 from data_transfer import adb_communicator, tba_communicator
 import utils
 
 
-def create_match_schedule_csv(local_file_path, tba_request_url):
-    """Creates a CSV file based on database's cached matches.
+@dataclass
+class LocalAndTabletPath:
+    local: str
+    tablet: str
 
-    Parameters: local_file_path (string): path to match_schedule.csv
-    tba_request_url: api_url for calling tba_communicator.tba_request()
-    :returns None on success, 1 on failure.
-    """
-    # Get matches from database
-    matches = tba_communicator.tba_request(tba_request_url)
-    # Don't try to create match schedule if matches returns a blank list
-    if matches == []:
-        return 1
-    # Filter out elimination matches; we don't use them for scouting
-    matches = [match for match in matches if match['comp_level'] == 'qm']
-    with open(local_file_path, 'w') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        # Write the match key and teams to the CSV file row by row,
-        # but using a slightly different format from what's on the tba_cache of the database.
+    def __repr__(self):
+        return f"LocalAndTabletPath(local={self.local}, \
+            tablet={self.tablet})"
+
+
+class MatchListGenerator:
+    def __init__(self, tba_request_url: str):
+        """Creates a CSV file based on database's cached matches.
+
+        Parameters: tba_request_url: api_url for calling tba_communicator.tba_request()
+        """
+        self.tba_request_url = tba_request_url
+
+        # Generate the data, save it as a list
+        self.match_schedule_file_data: List[str] = self.create_match_schedule_csv()
+        self.success = self.check()
+
+    def create_match_schedule_csv(self) -> List[str]:
+        """Get the data from tba_request and generate the match_schedule data"""
+        # Get matches from database
+        matches = tba_communicator.tba_request(self.tba_request_url)
+        # Don't try to create match schedule if matches returns a blank list
+        if matches == []:
+            raise "No matches on tba"
+        # Filter out elimination matches; we don't use them for scouting
+        matches = [match for match in matches if match['comp_level'] == 'qm']
+
+        # Make local list to append to
+        local_match_schedule_file_data: List[str] = []
+
+        # Generate a match schedule line by line, and add it to a list
         for match in matches:
             # match['key'] has event code, and underscore, then the match
             # example: '2019carv_qm118'
@@ -49,40 +69,87 @@ def create_match_schedule_csv(local_file_path, tba_request_url):
                     team_prefix = 'R-'
                 teams = [team_prefix + team.split('frc')[1] for team in teams]
                 new_row += teams
-            csv_writer.writerow(new_row)
-        utils.log_info(f'Match schedule created at {local_file_path}, url is {tba_request_url}')
+                local_match_schedule_file_data.append(new_row)
+        return local_match_schedule_file_data
+
+    def check(self):
+        """Check if the data was created correctly"""
+        checks = 0
+        if isinstance(self.match_schedule_file_data, list):
+            checks += 1
+        if len(self.match_schedule_file_data) > 0:
+            checks += 1
+        if checks >= 2:
+            return True
+        else:
+            return False
+
+    def return_match_schedule_data(self) -> List[str]:
+        """Get the match schedule as a list in case writing is not needed"""
+        return self.match_schedule_file_data
+
+    def write(self, local_file_path: str):
+        """Write the list of matches to a file"""
+        with open(local_file_path, 'w') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            for match in self.match_schedule_file_data:
+                csv_writer.writerow(match)
+        print("Match schedule written!")
 
 
-def get_team_list():
-    """Returns team list from match schedule file."""
-    teams = set()
-    if not SEND_MATCH_SCHEDULE:
-        teams = tba_communicator.tba_request(f'event/{utils.TBA_EVENT_KEY}/teams/simple')
-        # TBA returns a dictionary of information about teams at the event, so extract team numbers
-        team_numbers = [team['team_number'] for team in teams]
-        return sorted(team_numbers)
-    # Create stream from local match schedule copy to prevent extra handling for csv.reader
-    with open(MATCH_SCHEDULE_LOCAL_PATH) as file:
-        # Create csv reader from match schedule
-        match_schedule_reader = csv.reader(file)
-        for match in match_schedule_reader:
-            # Filter match number from match schedule list
-            for team in match[1:]:
-                # Strip alliance color information from team number before adding it
-                teams.add(team[2:])
-    # Return a list of teams sorted by team number
-    return sorted(map(int, teams))
+class TeamListGenerator:
+    def __init__(self, match_schedule_local_path: str, send_match_schedule: bool):
+        self.send_match_schedule = send_match_schedule
+        self.team_list = self.get_team_list()
+        self.match_schedule_local_path = match_schedule_local_path
+
+    def get_team_list(self) -> List:
+        """Returns team list from match schedule file."""
+        teams = set()
+        if not self.send_match_schedule:
+            teams = tba_communicator.tba_request(f'event/{utils.TBA_EVENT_KEY}/teams/simple')
+            # TBA returns a dictionary of information about teams at the event, so extract team numbers
+            team_numbers = [team['team_number'] for team in teams]
+            return sorted(team_numbers)
+        # Create stream from local match schedule copy to prevent extra handling for csv.reader
+        with open(self.match_schedule_local_path, "r") as file:
+            # Create csv reader from match schedule
+            match_schedule_reader = csv.reader(file)
+            for match in match_schedule_reader:
+                # Filter match number from match schedule list
+                for team in match[1:]:
+                    # Strip alliance color information from team number before adding it
+                    teams.add(team[2:])
+        # Return a list of teams sorted by team number
+        return sorted(map(int, teams))
+
+    def check(self):
+        """Check if the data was created correctly"""
+        checks = 0
+        if isinstance(self.team_list, set):
+            checks += 1
+        if len(self.team_list) > 0:
+            checks += 1
+        if checks >= 2:
+            return True
+        else:
+            return False
+
+    def write(self, output_file_path: str):
+        """Writes team list to file."""
+        team_list = self.get_team_list()
+        with open(output_file_path, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(team_list)
 
 
-def write_team_list(output_file_path):
-    """Writes team list to file."""
-    team_list = get_team_list()
-    with open(output_file_path, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(team_list)
-
-
-def validate_file(device_id, local_file_path, tablet_file_path):
+def validate_file(
+    device_id: str,
+    local_file_path: str,
+    tablet_file_path: str,
+    local_match_schedule_hash: str,
+    local_team_list_hash: str,
+):
     """Validates that `local_file_path` file was successfully transferred.
 
     Compares the `tablet_file_path` on the tablet to the locally stored
@@ -91,101 +158,156 @@ def validate_file(device_id, local_file_path, tablet_file_path):
     Parameter 'device_id' is the serial number of the device
     """
     tablet_data = adb_communicator.get_tablet_file_path_hash(device_id, tablet_file_path)
-    if local_file_path == MATCH_SCHEDULE_LOCAL_PATH:
-        return tablet_data == LOCAL_MATCH_SCHEDULE_HASH
-    if local_file_path == TEAM_LIST_LOCAL_PATH:
-        return tablet_data == LOCAL_TEAM_LIST_HASH
+    if local_file_path == match_schedule_local_path:
+        return tablet_data == local_match_schedule_hash
+    if local_file_path == team_list_local_path:
+        return tablet_data == local_team_list_hash
     raise ValueError(f'File path {local_file_path} not recognized.')
 
 
-# Set paths to read from and write to
-MATCH_SCHEDULE_TABLET_PATH = '/storage/self/primary/Download/match_schedule.csv'
-MATCH_SCHEDULE_LOCAL_PATH = utils.create_file_path('data/match_schedule.csv')
-TEAM_LIST_TABLET_PATH = '/storage/self/primary/Download/team_list.csv'
-TEAM_LIST_LOCAL_PATH = utils.create_file_path('data/team_list.csv')
+class Sender:
+    def __init__(
+        self, match_schedule_paths: LocalAndTabletPath, team_list_paths: LocalAndTabletPath
+    ):
 
-print(f'You are working with the competition {utils.TBA_EVENT_KEY}. Is that right?')
-while True:
-    if input('Hit enter to continue, or Ctrl-C to exit:') == '':
-        break
+        # Setup paths
+        self.team_list_local_path = team_list_paths.local
+        self.team_list_tablet_path = team_list_paths.tablet
 
-# Match schedule must be created before local copy is loaded
-if (
-    create_match_schedule_csv(
-        MATCH_SCHEDULE_LOCAL_PATH, f'event/{utils.TBA_EVENT_KEY}/matches/simple'
+        self.match_schedule_local_path = match_schedule_paths.local
+        self.match_schedule_tablet_path = match_schedule_paths.tablet
+
+        self.devices_with_schedule = set()
+        self.devices_with_list = set()
+
+        self.devices = set(adb_communicator.get_attached_devices())
+
+    def confirm_prep_and_generation(self):
+        print(f'You are working with the competition {utils.TBA_EVENT_KEY}. Is that right?')
+        while True:
+            if input('Hit enter to continue, or Ctrl-C to exit:') == '':
+                break
+
+        # Match schedule must be created before local copy is loaded
+        match_list_generator = MatchListGenerator(f'event/{utils.TBA_EVENT_KEY}/matches/simple')
+        if match_list_generator.success:
+            self.send_match_schedule = False
+        else:
+            self.send_match_schedule = True
+
+        match_list_generator.write(self.match_schedule_local_path)
+
+        team_list_generator = TeamListGenerator(
+            self.match_schedule_local_path, self.send_match_schedule
+        )
+
+        team_list_generator.write(self.team_list_local_path)
+
+        # LOCAL_MATCH_SCHEDULE contains the text of the match_schedule.csv file, which we compare with the
+        # output of cat (run on tablets through adb shell) for file validations.
+        if self.send_match_schedule:
+            with open(self.match_schedule_local_path, 'rb') as match_schedule_file:
+                # Store sha256 sum of match schedule
+                local_match_schedule_hash = hashlib.sha256(match_schedule_file.read()).hexdigest()
+        # Team list must be created after local match schedule copy is loaded
+        team_list_generator.write(self.team_list_local_path)
+        with open(self.team_list_local_path, 'rb') as team_list_file:
+            # Store sha256 sum of team list
+            local_team_list_hash = hashlib.sha256(team_list_file.read()).hexdigest()
+
+    def send(self):
+        print(f'Attempting to send:\n"{self.team_list_local_path}"')
+        if self.send_match_schedule:
+            print(f'{self.match_schedule_local_path}"\n')
+        else:
+            print(f'Match Schedule for "{utils.TBA_EVENT_KEY}" not available')
+
+        while True:
+            # Wait for USB connection to initialize
+            time.sleep(0.1)
+            for device in self.devices:
+                device_name = adb_communicator.DEVICE_SERIAL_NUMBERS[device]
+                if device not in self.devices_with_schedule and self.send_match_schedule:
+                    print(
+                        f'\nAttempting to load {self.match_schedule_local_path} onto {device_name}'
+                    )
+                    if adb_communicator.push_file(
+                        device,
+                        self.match_schedule_local_path,
+                        self.match_schedule_tablet_path,
+                        validate_file,
+                    ):
+                        self.devices_with_schedule.add(device)
+                        print(f'Loaded {self.match_schedule_local_path} onto {device_name}')
+                    else:
+                        # Give both serial number and device name in warning
+                        utils.log_warning(
+                            f'FAILED sending {self.match_schedule_local_path} to {device_name} ({device})'
+                        )
+                if device not in self.devices_with_list:
+                    print(f'\nAttempting to load {self.team_list_local_path} onto {device_name}')
+                    if adb_communicator.push_file(
+                        device, self.team_list_local_path, self.team_list_tablet_path, validate_file
+                    ):
+                        self.devices_with_list.add(device)
+                        print(f'Loaded {self.team_list_local_path} to {device_name}')
+                    else:
+                        # Give both serial number and device name in warning
+                        utils.log_warning(
+                            f'FAILED sending {self.team_list_local_path} to {device_name} ({device})'
+                        )
+            # Update connected devices before checking if program should exit
+            self.devices = set(adb_communicator.get_attached_devices())
+            if (
+                self.devices == self.devices_with_schedule
+                and self.devices == self.devices_with_list
+            ):
+                # Print blank lines for visual distinction
+                print('\n')
+                # Schedule has been loaded onto all connected devices
+                if self.send_match_schedule:
+                    if len(self.devices_with_schedule) != 1:
+                        print(
+                            f'Match schedule loaded onto {len(self.devices_with_schedule)} devices.'
+                        )
+                    else:
+                        print('Match schedule loaded onto 1 device.')
+                if len(self.devices_with_list) != 1:
+                    print(f'Team list loaded onto {len(self.devices_with_list)} devices.')
+                else:
+                    print('Team list loaded onto 1 device.')
+                    break
+
+
+def argument_parser():
+    parse = argparse.ArgumentParser()
+    parse.add_argument("-g", action='store_true', help="Generate match_schedule and team_list only")
+    parse.add_argument(
+        "-s", action='store_true', help="Generate and Send match_schedule and team_list"
     )
-    == 1
-):
-    SEND_MATCH_SCHEDULE = False
-else:
-    SEND_MATCH_SCHEDULE = True
+    return parse.parse_args()
 
-# LOCAL_MATCH_SCHEDULE contains the text of the match_schedule.csv file, which we compare with the
-# output of cat (run on tablets through adb shell) for file validations.
-if SEND_MATCH_SCHEDULE:
-    with open(MATCH_SCHEDULE_LOCAL_PATH, 'rb') as match_schedule_file:
-        # Store sha256 sum of match schedule
-        LOCAL_MATCH_SCHEDULE_HASH = hashlib.sha256(match_schedule_file.read()).hexdigest()
-# Team list must be created after local match schedule copy is loaded
-write_team_list(TEAM_LIST_LOCAL_PATH)
-with open(TEAM_LIST_LOCAL_PATH, 'rb') as team_list_file:
-    # Store sha256 sum of team list
-    LOCAL_TEAM_LIST_HASH = hashlib.sha256(team_list_file.read()).hexdigest()
 
-# Only upload schedules if file is ran, not imported
 if __name__ == '__main__':
-    # List of devices to which 'match_schedule.csv' has already been sent
-    DEVICES_WITH_SCHEDULE, DEVICES_WITH_LIST = set(), set()
-    DEVICES = set(adb_communicator.get_attached_devices())
+    # Set paths to read from and write to
+    match_schedule_tablet_path = '/storage/self/primary/Download/match_schedule.csv'
+    match_schedule_local_path = utils.create_file_path('data/match_schedule.csv')
+    team_list_tablet_path = '/storage/self/primary/Download/team_list.csv'
+    team_list_local_path = utils.create_file_path('data/team_list.csv')
 
-    print(f'Attempting to send:\n"{TEAM_LIST_LOCAL_PATH}"')
-    if SEND_MATCH_SCHEDULE:
-        print(f'{MATCH_SCHEDULE_LOCAL_PATH}"\n')
+    # Make paths object
+    match_schedule_paths = LocalAndTabletPath(match_schedule_local_path, match_schedule_tablet_path)
+    team_list_paths = LocalAndTabletPath(team_list_local_path, team_list_tablet_path)
+
+    # Send the files over
+    sender = Sender(match_schedule_paths, team_list_paths)
+
+    args = argument_parser()
+
+    if args.g:
+        sender.confirm_prep_and_generation()
+    elif args.s:
+        sender.confirm_prep_and_generation()
+        sender.send()
     else:
-        print(f'Match Schedule for "{utils.TBA_EVENT_KEY}" not available')
-
-    while True:
-        # Wait for USB connection to initialize
-        time.sleep(0.1)
-        for device in DEVICES:
-            device_name = adb_communicator.DEVICE_SERIAL_NUMBERS[device]
-            if device not in DEVICES_WITH_SCHEDULE and SEND_MATCH_SCHEDULE:
-                print(f'\nAttempting to load {MATCH_SCHEDULE_LOCAL_PATH} onto {device_name}')
-                if adb_communicator.push_file(
-                    device, MATCH_SCHEDULE_LOCAL_PATH, MATCH_SCHEDULE_TABLET_PATH, validate_file
-                ):
-                    DEVICES_WITH_SCHEDULE.add(device)
-                    print(f'Loaded {MATCH_SCHEDULE_LOCAL_PATH} onto {device_name}')
-                else:
-                    # Give both serial number and device name in warning
-                    utils.log_warning(
-                        f'FAILED sending {MATCH_SCHEDULE_LOCAL_PATH} to {device_name} ({device})'
-                    )
-            if device not in DEVICES_WITH_LIST:
-                print(f'\nAttempting to load {TEAM_LIST_LOCAL_PATH} onto {device_name}')
-                if adb_communicator.push_file(
-                    device, TEAM_LIST_LOCAL_PATH, TEAM_LIST_TABLET_PATH, validate_file
-                ):
-                    DEVICES_WITH_LIST.add(device)
-                    print(f'Loaded {TEAM_LIST_LOCAL_PATH} to {device_name}')
-                else:
-                    # Give both serial number and device name in warning
-                    utils.log_warning(
-                        f'FAILED sending {TEAM_LIST_LOCAL_PATH} to {device_name} ({device})'
-                    )
-        # Update connected devices before checking if program should exit
-        DEVICES = set(adb_communicator.get_attached_devices())
-        if DEVICES == DEVICES_WITH_SCHEDULE and DEVICES == DEVICES_WITH_LIST:
-            # Print blank lines for visual distinction
-            print('\n')
-            # Schedule has been loaded onto all connected devices
-            if SEND_MATCH_SCHEDULE:
-                if len(DEVICES_WITH_SCHEDULE) != 1:
-                    print(f'Match schedule loaded onto {len(DEVICES_WITH_SCHEDULE)} devices.')
-                else:
-                    print('Match schedule loaded onto 1 device.')
-            if len(DEVICES_WITH_LIST) != 1:
-                print(f'Team list loaded onto {len(DEVICES_WITH_LIST)} devices.')
-            else:
-                print('Team list loaded onto 1 device.')
-            break
+        print("Run send_match_schedule.py --help")
