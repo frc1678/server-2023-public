24c11fff31adff933953a3d746f411480ff7ef71
moltenL <68791398+moltenL@users.noreply.github.com>
2021-03-13 10:21:26 -0800

    Update objective team calculations to base calculations (#495)

--- src/calculations/obj_team.py
+++ src/calculations/obj_team.py
@@ -1,77 +1,95 @@
 #!/usr/bin/env python3
 """Calculate objective team data from Team in Match (TIM) data."""
 
-import os
-import yaml
-
-
-current_directory = os.path.dirname(os.path.realpath(__file__))
-parent_directory = os.path.dirname(current_directory)
-from data_transfer import local_database_communicator as ldc
-
 import utils
 from typing import List, Dict
+from src.calculations import base_calculations
 
 
-def calculate_averages(tims: List[Dict], schema_for_averages: Dict):
-    """Creates a dictionary of calculated averages, called team_info,
-    where the keys are the names of the calculations, and the values are the results
-    """
-    team_info = {}
-    for calculation, schema in schema_for_averages.items():
-        # Find tims that meet required data field:
-        tim_action_counts = []
-        for tim in tims:
-            # Gets the total number of actions for a single tim
-            tim_action_counts.append(sum([tim[tim_field] for tim_field in schema['tim_fields']]))
-        if schema['type'] in ['int', 'float']:
-            average = utils.avg(tim_action_counts)
-            average = STR_TYPES[schema['type']](average)
-        else:
-            raise TypeError(f'{calculation} should be a number in calc obj team schema')
-        team_info[calculation] = average
-    return team_info
+class OBJTeamCalc(base_calculations.BaseCalculations):
+    """Runs OBJ Team calculations"""
+    # Get the last section of each entry (so foo.bar.baz becomes baz)
+    OBJ_TEAM_SCHEMA = utils.unprefix_schema_dict(
+        utils.read_schema('schema/calc_obj_team_schema.yml')
+    )
+    STR_TYPES = {'str': str, 'float': float, 'int': int}
 
+    def __init__(self, server):
+        """Overrides watched collections, passes server object"""
+        super().__init__(server)
+        self.watched_collections = ['obj_tim']
+        # Used for converting to a type that is given as a string
 
-def calculate_counts(tims, schema_for_counts):
-    """Creates a dictionary of calculated counts, called team_info,
-    where the keys are the names of the calculations, and the values are the results
-    """
-    team_info = {}
-    for calculation, schema in schema_for_counts.items():
+    def calculate_averages(self, tims: List[Dict]):
+        """Creates a dictionary of calculated averages, called team_info,
+        where the keys are the names of the calculations, and the values are the results
+        """
+        team_info = {}
+        for calculation, schema in self.OBJ_TEAM_SCHEMA['averages'].items():
+            # Find tims that meet required data field:
+            tim_action_counts = []
+            for tim in tims:
+                # Gets the total number of actions for a single tim
+                tim_action_counts.append(sum([tim[tim_field.split(".")[1]] for tim_field in schema['tim_fields']]))
+            if schema['type'] in ['int', 'float']:
+                average = self.avg(tim_action_counts)
+                average = self.STR_TYPES[schema['type']](average)
+            else:
+                raise TypeError(f'{calculation} should be a number in calc obj team schema')
+            team_info[calculation] = average
+        return team_info
+
+    def filter_tims_for_counts(self, tims: List[Dict], schema):
+        """Filters tims based on schema for count calculations"""
         tims_that_meet_filter = tims
         for key, value in schema['tim_fields'].items():
-            if key == 'not':
+            if key != 'not':
+                # Checks that the TIMs in their given field meet the filter
+                tims_that_meet_filter = list(filter(lambda tim: tim[key] == value, tims_that_meet_filter))
+            else:
                 # not_field expects the output to be anything but the given filter
                 # not_value is the filter that not_field shouldn't have
                 for not_field, not_value in value.items():
                     # Checks that the TIMs in the 'not' field are anything other than the filter
-                    tims_that_meet_filter = list(
-                        filter(
-                            lambda tim: tim.get(not_field, not_value) != not_value,
-                            tims_that_meet_filter,
-                        )
-                    )
-            else:
-                # Checks that the TIMs in their given field meet the filter
-                tims_that_meet_filter = list(
-                    filter(lambda tim: tim[key] == value, tims_that_meet_filter)
-                )
-        team_info[calculation] = STR_TYPES[schema['type']](len(tims_that_meet_filter))
-    return team_info
+                    tims_that_meet_filter = list(filter(lambda tim: tim.get(not_field, not_value) != not_value,
+                                                        tims_that_meet_filter))
+        return tims_that_meet_filter
 
+    def calculate_counts(self, tims: List[Dict]):
+        """Creates a dictionary of calculated counts, called team_info,
+        where the keys are the names of the calculations, and the values are the results
+        """
+        team_info = {}
+        for calculation, schema in self.OBJ_TEAM_SCHEMA['counts'].items():
+            tims_that_meet_filter = self.filter_tims_for_counts(tims, schema)
+            team_info[calculation] = self.STR_TYPES[schema['type']](len(tims_that_meet_filter))
+        return team_info
 
-def calculate_obj_team(team):
-    """Calculate data for given team using objective calculated TIMs"""
-    # list of TIMs that the team has been in:
-    tims = ldc.read_dataset('processed.calc_obj_tim', team_number=team)
-    team_info = calculate_averages(tims, SCHEMA['averages'])
-    team_info.update(calculate_counts(tims, SCHEMA['counts']))
-    return team_info
+    def update_team_calcs(self, teams: list) -> list:
+        """Calculate data for given team using objective calculated TIMs"""
 
+        obj_team_updates = {}
+        for team in teams:
+            # Load team data from database
+            obj_tims = self.server.db.find('obj_tim', **{'team_number': team})
+            team_data = self.calculate_averages(obj_tims)
+            team_data['team_number'] = team
+            team_data.update(self.calculate_counts(obj_tims))
+            obj_team_updates[team] = team_data
+        return list(obj_team_updates.values())
 
-# Used for converting to a type that is given as a string
-STR_TYPES = {'str': str, 'float': float, 'int': int}
-
-with open(utils.create_file_path('schema/calc_obj_team_schema.yml')) as file:
-    SCHEMA = yaml.load(file, yaml.Loader)
+    def run(self):
+        """Executes the OBJ Team calculations"""
+        # Get oplog entries
+        entries = self.entries_since_last()
+        teams = set()
+        # Check if changes need to be made to teams
+        if self.entries_since_last() is not None:
+            for entry in entries:
+                # Prevents error from not having a team num
+                if 'team_number' in entry['o'].keys():
+                    teams.add(entry['o']['team_number'])
+        for update in self.update_team_calcs(list(teams)):
+            self.server.db.update_document(
+                'obj_team', update, {'team_number': update['team_number']}
+            )
--- tests/calculations/test_obj_team.py
+++ tests/calculations/test_obj_team.py
@@ -2,91 +2,181 @@
 # Copyright (c) 2019 FRC Team 1678: Citrus Circuits
 import pytest
 from src.calculations import obj_team
+from src.server import Server
 
 
-def test_averaging_low_and_high_goals_together():
-    """Tests calculate_averages function from src/calculations/obj_team.py
+@pytest.mark.clouddb
+class TestOBJTeamCalc:
+    def setup_method(self, method):
+        self.test_server = Server()
+        self.test_calc = obj_team.OBJTeamCalc(self.test_server)
 
-    In this case, tests only the calculations auto_avg_balls_total and tele_avg_balls_total
-    """
-    tims = [
-        {'auto_balls_low': 1, 'auto_balls_high': 2, 'tele_balls_low': 3, 'tele_balls_high': 4},
-        {
-            'auto_balls_low': 5,
-            'auto_balls_high': 6,
-            'tele_balls_low': 7,
-            'tele_balls_high': 8,
-            'the coolest': 'Eugene Chen',
-        },
-    ]
-    schema_for_averages = {
-        'auto_avg_balls_total': {
-            'tim_fields': ['auto_balls_low', 'auto_balls_high'],
-            'type': 'float',
-        },
-        'tele_avg_balls_total': {
-            'tim_fields': ['tele_balls_low', 'tele_balls_high'],
-            'type': 'float',
-        },
-    }
-    expected_output = {'auto_avg_balls_total': 7.0, 'tele_avg_balls_total': 11.0}
-    assert obj_team.calculate_averages(tims, schema_for_averages) == expected_output
+    def test___init__(self):
+        """Test if attributes are set correctly"""
+        assert self.test_calc.watched_collections == ['obj_tim']
+        assert self.test_calc.server == self.test_server
 
+    def test_averages(self):
+        """Tests calculate_averages function from src/calculations/obj_team.py"""
+        tims = [
+            {
+                'auto_balls_low': 1,
+                'auto_balls_high': 2,
+                'tele_balls_low': 3,
+                'tele_balls_high': 4,
+                'incap': 0.0
+            },
+            {
+                'auto_balls_low': 5,
+                'auto_balls_high': 6,
+                'tele_balls_low': 7,
+                'tele_balls_high': 8,
+                'incap': 15.0,
+                'the coolest': 'Eugene Chen',
+            },
 
-def test_calculating_avg_incap_time():
-    # The data field 'incap' in TIMs is the average number of seconds the robot spent incapacitated
-    tims = [{'incap': 0.0}, {'incap': 0.0}, {'incap': 118}, {'incap': 0}]
-    schema_for_averages = {'avg_incap_time': {'tim_fields': ['incap'], 'type': 'float'}}
-    expected_output = {'avg_incap_time': 29.5}
-    assert obj_team.calculate_averages(tims, schema_for_averages) == expected_output
+        ]
 
+        expected_output = {'auto_avg_balls_total': 7.0,
+                           'tele_avg_balls_total': 11.0,
+                           'auto_avg_balls_low': 3.0,
+                           'auto_avg_balls_high': 4.0,
+                           'tele_avg_balls_low': 5.0,
+                           'tele_avg_balls_high': 6.0,
+                           'avg_incap_time': 7.5}
+        assert self.test_calc.calculate_averages(tims) == expected_output
 
-def test_misspelled_data_field():
-    tims = [{'incap': 0.0}, {'incap': 0.0}, {'incap': 118}, {'incap': 0}]
-    schema_for_averages = {'avg_incap_time': {'tim_fields': ['incapacitated'], 'type': 'float'}}
-    # Since the data field was called 'incapacitated' in the schema and 'incap' in the actual TIMs,
-    # this should throw a KeyError
-    expected_output = {'avg_incap_time': 29.5}
-    with pytest.raises(KeyError):
-        assert obj_team.calculate_averages(tims, schema_for_averages) == expected_output
+    def test_counts(self):
+        """Tests calculate_counts function from src/calculations/obj_team.py"""
+        tims = [
+            {'control_panel_rotation': True, 'control_panel_position': True, 'climb_time': 0},
+            {'control_panel_rotation': True, 'control_panel_position': False, 'climb_time': 20},
+            {'control_panel_rotation': False, 'control_panel_position': False, 'climb_time': 5},
+        ]
 
+        expected_output = {
+            'tele_cp_rotation_successes': 2,
+            'tele_cp_position_successes': 1,
+            'climb_all_attempts': 2,
+        }
+        assert self.test_calc.calculate_counts(tims) == expected_output
 
-def test_counts():
-    """Just a normal test case for the function obj_team.calculate_counts as it is supposed to be used"""
-    tims = [
-        {'control_panel_rotation': True, 'control_panel_position': True, 'climb_time': 0},
-        {'control_panel_rotation': True, 'control_panel_position': False, 'climb_time': 20},
-        {'control_panel_rotation': False, 'control_panel_position': False, 'climb_time': 5},
-    ]
-    schema_for_counts = {
-        'tele_cp_rotation_successes': {
-            'tim_fields': {'control_panel_rotation': True},
-            'type': 'int',
-        },
-        'tele_cp_position_successes': {
-            'tim_fields': {'control_panel_position': True},
-            'type': 'int',
-        },
-        'climb_all_attempts': {'tim_fields': {'not': {'climb_time': 0}}, 'type': 'int'},
-    }
-    expected_output = {
-        'tele_cp_rotation_successes': 2,
-        'tele_cp_position_successes': 1,
-        'climb_all_attempts': 2,
-    }
-    assert obj_team.calculate_counts(tims, schema_for_counts) == expected_output
-
-
-def test_wonky_schema_for_counts():
-    """Since the schema for this is weird, make sure it fails if the schema is extra wonky"""
-    tims = [
-        {'control_panel_rotation': True, 'control_panel_position': True, 'climb_time': 0},
-        {'control_panel_rotation': True, 'control_panel_position': False, 'climb_time': 20},
-        {'control_panel_rotation': False, 'control_panel_position': False, 'climb_time': 5},
-    ]
-    schema_for_counts = {
-        'climb_all_attempts': {'tim_fields': {'climb_success': True}, 'type': 'int'}
-    }
-    expected_output = {'climb_all_attempts': 2}
-    with pytest.raises(KeyError):
-        assert obj_team.calculate_counts(tims, schema_for_counts) == expected_output
+    def test_run(self):
+        """Tests run function from src/calculations/obj_team.py"""
+        obj_tims = [
+            {
+                'auto_balls_low': 66,
+                'auto_balls_high': 61,
+                'tele_balls_low': 18,
+                'tele_balls_high': 45,
+                'control_panel_rotation': True,
+                'control_panel_position': False,
+                'incap': 14,
+                'climb_time': 21,
+                'confidence_rating': 30,
+                'team_number': 973,
+                'match_number': 1,
+            },
+            {
+                'auto_balls_low': 30,
+                'auto_balls_high': 60,
+                'tele_balls_low': 56,
+                'tele_balls_high': 1,
+                'control_panel_rotation': True,
+                'control_panel_position': True,
+                'incap': 22,
+                'climb_time': 3,
+                'confidence_rating': 68,
+                'team_number': 973,
+                'match_number': 2,
+            },
+            {
+                'auto_balls_low': 84,
+                'auto_balls_high': 92,
+                'tele_balls_low': 40,
+                'tele_balls_high': 68,
+                'control_panel_rotation': False,
+                'control_panel_position': False,
+                'incap': 18,
+                'climb_time': 0,
+                'confidence_rating': 2,
+                'team_number': 973,
+                'match_number': 3,
+            },
+            {
+                'auto_balls_low': 47,
+                'auto_balls_high': 76,
+                'tele_balls_low': 34,
+                'tele_balls_high': 81,
+                'control_panel_rotation': True,
+                'control_panel_position': True,
+                'incap': 17,
+                'climb_time': 46,
+                'confidence_rating': 31,
+                'team_number': 1678,
+                'match_number': 1,
+            },
+            {
+                'auto_balls_low': 25,
+                'auto_balls_high': 54,
+                'tele_balls_low': 81,
+                'tele_balls_high': 59,
+                'control_panel_rotation': True,
+                'control_panel_position': True,
+                'incap': 93,
+                'climb_time': 35,
+                'confidence_rating': 14,
+                'team_number': 1678,
+                'match_number': 2,
+            },
+            {
+                'auto_balls_low': 54,
+                'auto_balls_high': 50,
+                'tele_balls_low': 89,
+                'tele_balls_high': 73,
+                'control_panel_rotation': True,
+                'control_panel_position': True,
+                'incap': 16,
+                'climb_time': 97,
+                'confidence_rating': 77,
+                'team_number': 1678,
+                'match_number': 3,
+            },
+        ]
+        expected_results = [
+            {
+                'team_number': 973,
+                'auto_avg_balls_total': 131.0,
+                'tele_avg_balls_total': 76.0,
+                'auto_avg_balls_low': 60.0,
+                'auto_avg_balls_high': 71.0,
+                'tele_avg_balls_low': 38.0,
+                'tele_avg_balls_high': 38.0,
+                'avg_incap_time': 18.0,
+                'tele_cp_rotation_successes': 2,
+                'tele_cp_position_successes': 1,
+                'climb_all_attempts': 2
+            },
+            {
+                "team_number": 1678,
+                'auto_avg_balls_total': 102.0,
+                'tele_avg_balls_total': 139.0,
+                'auto_avg_balls_low': 42.0,
+                'auto_avg_balls_high': 60.0,
+                'tele_avg_balls_low': 68.0,
+                'tele_avg_balls_high': 71.0,
+                'avg_incap_time': 42.0,
+                'tele_cp_rotation_successes': 3,
+                'tele_cp_position_successes': 3,
+                'climb_all_attempts': 3
+            },
+        ]
+        self.test_server.db.insert_documents('obj_tim', obj_tims)
+        self.test_calc.run()
+        result = self.test_server.db.find('obj_team')
+        assert len(result) == 2
+        for document in result:
+            del document['_id']
+            assert document in expected_results
+            # Removes the matching expected result to protect against duplicates from the calculation
+            expected_results.remove(document)
