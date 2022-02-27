fcf8cc205d71f75ba64c101a0729df96d18442de
LudiWang85454 <43869782+LudiWang85454@users.noreply.github.com>
2022-02-27 10:09:08 -0800

    Send robot images to viewer phones (#692)
    * Create script
    
    * Format
    
    * Move code into functions
    
    * Newline
    
    * Format
    
    * Fix variable names
    
    * Format

---
+++ src/send_viewer_images.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 FRC Team 1678: Citrus Circuits
+"""Send pit robot images to the Download folder of the viewer phones."""
+
+import os
+import re
+from typing import Dict
+import utils
+from data_transfer import adb_communicator
+
+IMAGE_PATH_PATTERN = re.compile(r"([0-9]+)_(full_robot|drivetrain|mechanism_[0-9]+)\.jpg")
+
+
+def find_robot_images() -> Dict[str, str]:
+    """Iterate through the data/tablets folder to find all robot images."""
+    paths = {}
+    for device in os.listdir(utils.create_file_path("data/tablets")):
+        # Check folder names to only look for images from phones
+        if device not in adb_communicator.PHONE_SERIAL_NUMBERS:
+            continue
+        device_dir = utils.create_file_path(f"data/tablets/{device}/")
+        for file in os.listdir(device_dir):
+            full_local_path = os.path.join(device_dir, file)
+            # Tries to match the file name with the regular expression
+            result = re.fullmatch(IMAGE_PATH_PATTERN, file)
+            # If the regular expression matched
+            if result:
+                paths.update({file: full_local_path})
+    return paths
+
+
+def send_images() -> None:
+    """Push images to the Download folder of the viewer phones."""
+    for device in adb_communicator.get_attached_devices():
+        if device not in adb_communicator.PHONE_SERIAL_NUMBERS:
+            continue
+        images_sent = 0
+        for filename, full_path in find_robot_images().items():
+            adb_communicator.push_file(device, full_path, f'storage/emulated/0/Download/{filename}')
+            images_sent += 1
+        print(f'Sent {images_sent} photos to {adb_communicator.DEVICE_SERIAL_NUMBERS[device]}')
+
+
+if __name__ == '__main__':
+    send_images()
