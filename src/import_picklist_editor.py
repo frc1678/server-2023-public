ae9b5d66214f8cde553eb491c3d94617050fc7d7
calvin-laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-04-07 12:40:10 -0700

    Import picklist dnp and remove second pick (#757)
    * remove second pick from import picklist
    
    * add dnp support to import picklist
    
    * a bit of refactoring for import picklist
    
    * format

--- src/import_picklist_editor.py
+++ src/import_picklist_editor.py
@@ -1,5 +1,6 @@
 from os.path import exists
 from data_transfer.database import Database
+from send_device_jsons import get_team_list
 from utils import read_csv_file
 
 if __name__ == "__main__":
@@ -15,13 +16,25 @@ if __name__ == "__main__":
 
     for row in csvdata:
         # Check if the team number and the positions are numbers
-        if row[0].isnumeric() and row[1].isnumeric() and row[2].isnumeric():
+        if row[0].isnumeric() and row[1].isnumeric():
             # Add the team to the list of teams
-            teams.append(
-                {"team_number": int(row[0]), "first_rank": int(row[1]), "second_rank": int(row[2])}
-            )
+            teams.append({"team_number": int(row[0]), "rank": int(row[1])})
 
+    # Get the list of all the teams from the event
+    team_list = get_team_list()
+
+    # Just gets the team numbers from picklist
+    picklist_team_numbers = list(map(lambda x: x["team_number"], teams))
+
+    # For each team in the list of all competition teams, if it is not in the list of teams in the picklist, add it to the list
+    dnp = [item for item in team_list if item not in picklist_team_numbers]
+
+    # Add each pick list object to the database
     for e in teams:
-        # Add each pick list object to the database
-        DATABASE.update_document("picklist", e, {"team_number": e["team_number"]})
-        # print(e)
+        DATABASE.update_document("picklist", {**e, "dnp": False}, {"team_number": e["team_number"]})
+
+    # Add each dnp team to the database
+    for e in dnp:
+        DATABASE.update_document(
+            "picklist", {"dnp": True, "team_number": e, "rank": -1}, {"team_number": e}
+        )
