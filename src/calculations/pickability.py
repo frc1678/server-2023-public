ece4ddb7249ee5abe030355d1306df5d4efd1a39
Rohaansandhu <81888230+Rohaansandhu@users.noreply.github.com>
2023-02-27 20:27:20 -0800

    Update pickability for max second pickability (#987)
    * Update pickability to make a max out of the second pickabilities
    
    * Reformat files
    
    * Update Schema

--- src/calculations/pickability.py
+++ src/calculations/pickability.py
@@ -10,16 +10,14 @@ class PickabilityCalc(base_calculations.BaseCalculations):
 
     def __init__(self, server):
         super().__init__(server)
-        self.pickability_schema = utils.read_schema("schema/calc_pickability_schema.yml")[
-            "calculations"
-        ]
+        self.pickability_schema = utils.read_schema("schema/calc_pickability_schema.yml")
         self.get_watched_collections()
 
     def get_watched_collections(self):
         """Reads from the schema file to generate the correct watched collections"""
         self.watched_collections = set()
-        for calc in self.pickability_schema:
-            for sub_calc in self.pickability_schema[calc]:
+        for calc in self.pickability_schema["calculations"]:
+            for sub_calc in self.pickability_schema["calculations"][calc]:
                 if "." in sub_calc:
                     self.watched_collections.add(sub_calc.split(".")[0])
 
@@ -31,7 +29,7 @@ class PickabilityCalc(base_calculations.BaseCalculations):
         returns the weighted sum
         """
         weighted_sum = 0
-        for calc, weighted_value in self.pickability_schema[
+        for calc, weighted_value in self.pickability_schema["calculations"][
             calc_name
         ].items():  # Datapoints of first or second pickability
             if "." not in calc:  # Ignore 'type' in schema
@@ -41,6 +39,21 @@ class PickabilityCalc(base_calculations.BaseCalculations):
                 return  # Can't calculate this pickability
             weighted_sum += team_data[collection][datapoint] * weighted_value
         return weighted_sum
+
+    def calculate_max_pickability(self, calc_name, updates):
+        """Returns the max between offensive and defensive second pickability
+        //TODO Remove unnecessary variables and make the code cleaner"""
+        # Only used for overall_second_pickability
+        offensive_second_pickability = None
+        defensive_second_pickability = None
+        for update in updates:
+            if "offensive_second_pickability" in update:
+                offensive_second_pickability = update["offensive_second_pickability"]
+            if "defensive_second_pickability" in update:
+                defensive_second_pickability = update["defensive_second_pickability"]
+        if not offensive_second_pickability or not defensive_second_pickability:
+            return  # Can't calculate pickability without both
+        return max(offensive_second_pickability, defensive_second_pickability)
 
     def update_pickability(self):
         """Creates updated pickability documents"""
@@ -53,8 +66,15 @@ class PickabilityCalc(base_calculations.BaseCalculations):
                 if (query := self.server.db.find(collection, {"team_number": team})) != []:
                     team_data[collection] = query[0]
             update = {"team_number": team}
-            for calc_name in self.pickability_schema:
+            for calc_name in self.pickability_schema["calculations"]:
                 value = self.calculate_pickability(calc_name, team_data)
+                if value is None:
+                    log.error(f"{calc_name} could not be calculated for team: {team}")
+                    continue
+                update[calc_name] = value
+                updates.append(update)
+            for calc_name in self.pickability_schema["max_calculations"]:
+                value = self.calculate_max_pickability(calc_name, updates)
                 if value is None:
                     log.error(f"{calc_name} could not be calculated for team: {team}")
                     continue
@@ -64,6 +84,7 @@ class PickabilityCalc(base_calculations.BaseCalculations):
 
     def run(self) -> None:
         """Detects when and for which teams to calculate pickabilty"""
+
         # Finds oplog entries in the watched collections
         entries = self.entries_since_last()
         if entries == []:
--- tests/calculations/test_pickability.py
+++ tests/calculations/test_pickability.py
@@ -26,12 +26,13 @@ FAKE_SCHEMA = {
             "test.datapoint2": 1,
             "test2.datapoint1": 2,
         },
+    },
+    "max_calculations": {
         "overall_second_pickability": {
             "type": "float",
-            "test3.datapoint1": 0.5,
-            "test3.datapoint2": 0.5,
-        },
-    }
+            "fields": ["offensive_second_pickability", "defensive_second_pickability"],
+        }
+    },
 }
 
 
@@ -59,12 +60,10 @@ class TestPickability:
                 "useless": None,
             },
             "test2": {"team_number": "0", "datapoint1": 3, "useless": None},
-            "test3": {"team_number": "0", "datapoint1": 15, "datapoint2": 17},
         }
         assert test_calc.calculate_pickability("first_pickability", calc_data) == 6
         assert test_calc.calculate_pickability("offensive_second_pickability", calc_data) == 15
         assert test_calc.calculate_pickability("defensive_second_pickability", calc_data) == 17
-        assert test_calc.calculate_pickability("overall_second_pickability", calc_data) == 16
         assert test_calc.calculate_pickability("first_pickability", {}) is None
         # Check that if the datapoint is missing that it correctly returns None
         calc_data = {
@@ -80,27 +79,69 @@ class TestPickability:
     @staticmethod
     @mock.patch("utils.read_schema", return_value=FAKE_SCHEMA)
     @mock.patch("server.Server.ask_calc_all_data", return_value=False)
+    def test_calculate_max_pickability(mock, calc_all_data_mock):
+        test_calc = pickability.PickabilityCalc(Server())
+        calc_data = [
+            {"first_pickability": 6},
+            {"offensive_second_pickability": 5},
+            {"defensive_second_pickability": 9},
+        ]
+        calc_data_2 = [
+            {"first_pickability": 6},
+            {"offensive_second_pickability": 5},
+            {"defensive_second_pickability": 5},
+        ]
+        calc_data_3 = [
+            {"first_pickability": 6},
+            {"offensive_second_pickability": 10},
+            {"defensive_second_pickability": 9},
+        ]
+        calc_data_4 = [
+            {"first_pickability": 6},
+            {"offensive_second_pickability": 5},
+        ]
+        assert test_calc.calculate_max_pickability("overall_second_pickability", calc_data) == 9
+        assert test_calc.calculate_max_pickability("overall_second_pickability", calc_data_2) == 5
+        assert test_calc.calculate_max_pickability("overall_second_pickability", calc_data_3) == 10
+        assert (
+            test_calc.calculate_max_pickability("overall_second_pickability", calc_data_4) is None
+        )
+
+    @staticmethod
+    @mock.patch("utils.read_schema", return_value=FAKE_SCHEMA)
+    @mock.patch("server.Server.ask_calc_all_data", return_value=False)
     def test_run(calc_all_data_mock, schema_mock):
         server_obj = Server()
         test_calc = pickability.PickabilityCalc(server_obj)
+
         # This is not enough to do a pickability calc, it needs the test2 datapoint
         server_obj.db.insert_documents(
             "test", {"team_number": "0", "datapoint1": 1, "datapoint2": 5}
         )
         test_calc.run()
         assert server_obj.db.find("pickability") == []
+
         # Insert all the required data
         server_obj.db.insert_documents(
-            "test2", {"team_number": "0", "datapoint1": 1, "datapoint2": 5}
+            "test2", {"team_number": "0", "datapoint1": 1, "datapoint2": 3}
         )
         test_calc.run()
         result = server_obj.db.find("pickability")
-        # Check that a result was added
-        assert result
+        del result[0]["_id"]
+        assert result[0] == {
+            "team_number": "0",
+            "defensive_second_pickability": 12,
+            "first_pickability": 7,
+            "offensive_second_pickability": 10,
+            "overall_second_pickability": 12,
+        }
+
+        # Test updating the function
         server_obj.db.delete_data("test2")
         server_obj.db.insert_documents(
             "test2", {"team_number": "0", "datapoint1": 2000000, "datapoint2": 20}
         )
         test_calc.run()
         new_result = server_obj.db.find("pickability")
+        del new_result[0]["_id"]
         assert result != new_result
