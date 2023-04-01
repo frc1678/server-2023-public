e6c421a60f31d5cb0a354c4c8f0daf10fcf14a95
Jeffrey Wang <48331629+realjef@users.noreply.github.com>
2023-04-01 10:18:17 -0700

    Calculate cc (#1042)
    * check if document alrdy exists
    
    * oops
    
    * combine ccs
    
    * d
    
    * black
    
    ---------
    
    Co-authored-by: Jeffrey Wang <jeffreywang@Jeffreys-MacBook-Air.local>

--- src/calculations/tba_team.py
+++ src/calculations/tba_team.py
@@ -73,9 +73,9 @@ class TBATeamCalc(base_calculations.BaseCalculations):
             out[name] = count
         return out
 
-    def calculate_foul_cc(self, precision: int = 2) -> Dict[str, float]:
+    def calculate_cc(self, cc_type, precision: int = 2) -> Dict[str, float]:
         """
-        Calculates the amount of foul points each team contributes.
+        Calculates the amount of foul/link points each team contributes.
 
         Calculated contribution (a.k.a. OPR) is a method of estimating the amount of something a team contributes to an alliance.
 
@@ -90,65 +90,48 @@ class TBATeamCalc(base_calculations.BaseCalculations):
         if matches_resp is None:
             matches_resp = {"data": tba_communicator.tba_request(matches_endpoint)}
         tba_matches = matches_resp.get("data", [])
-        cc_aims: List[CCEvent] = []
-        # For each AIM, add the foul points for the other alliance in a dictionary
-        # with the team numbers and foul points contributed
-        for match in tba_matches:
-            if match.get("score_breakdown", None) is None:
-                continue
-            cc_aims.append(
-                {
-                    "parties": utils.get_teams_in_match(match, "red"),
-                    "value": match["score_breakdown"]["blue"]["foulPoints"],
-                }
-            )
-            cc_aims.append(
-                {
-                    "parties": utils.get_teams_in_match(match, "blue"),
-                    "value": match["score_breakdown"]["red"]["foulPoints"],
-                }
-            )
+        if cc_type == "foul":
+            cc_aims: List[CCEvent] = []
+            # For each AIM, add the foul points for the other alliance in a dictionary
+            # with the team numbers and foul points contributed
+            for match in tba_matches:
+                if match.get("score_breakdown", None) is None:
+                    continue
+                cc_aims.append(
+                    {
+                        "parties": utils.get_teams_in_match(match, "red"),
+                        "value": match["score_breakdown"]["blue"]["foulPoints"],
+                    }
+                )
+                cc_aims.append(
+                    {
+                        "parties": utils.get_teams_in_match(match, "blue"),
+                        "value": match["score_breakdown"]["red"]["foulPoints"],
+                    }
+                )
 
-        solved = cc(cc_aims)
-        return solved
+            solved = cc(cc_aims)
+        if cc_type == "link":
+            cc_aims: List[CCEvent] = []
+            # For each AIM, add the link points for the other alliance in a dictionary
+            # with the team numbers and link points contributed
+            for match in tba_matches:
+                if match.get("score_breakdown", None) is None:
+                    continue
+                cc_aims.append(
+                    {
+                        "parties": utils.get_teams_in_match(match, "red"),
+                        "value": match["score_breakdown"]["red"]["linkPoints"],
+                    }
+                )
+                cc_aims.append(
+                    {
+                        "parties": utils.get_teams_in_match(match, "blue"),
+                        "value": match["score_breakdown"]["blue"]["linkPoints"],
+                    }
+                )
 
-    def calculate_link_cc(self, precision: int = 2) -> Dict[str, float]:
-        """
-        Calculates the amount of foul points each team contributes.
-
-        Calculated contribution (a.k.a. OPR) is a method of estimating the amount of something a team contributes to an alliance.
-
-        It uses a numpy matrix and calculates a least squares solution for each team.
-
-        See Also
-        ---------
-        TBA Blog post discussing OPR https://blog.thebluealliance.com/2017/10/05/the-math-behind-opr-an-introduction/
-        """
-        matches_endpoint = f"event/{Server.TBA_EVENT_KEY}/matches"
-        matches_resp = self.server.db.get_tba_cache(matches_endpoint)
-        if matches_resp is None:
-            matches_resp = {"data": tba_communicator.tba_request(matches_endpoint)}
-        tba_matches = matches_resp.get("data", [])
-        cc_aims: List[CCEvent] = []
-        # For each AIM, add the foul points for the other alliance in a dictionary
-        # with the team numbers and foul points contributed
-        for match in tba_matches:
-            if match.get("score_breakdown", None) is None:
-                continue
-            cc_aims.append(
-                {
-                    "parties": utils.get_teams_in_match(match, "red"),
-                    "value": match["score_breakdown"]["red"]["linkPoints"],
-                }
-            )
-            cc_aims.append(
-                {
-                    "parties": utils.get_teams_in_match(match, "blue"),
-                    "value": match["score_breakdown"]["blue"]["linkPoints"],
-                }
-            )
-
-        solved = cc(cc_aims)
+            solved = cc(cc_aims)
         return solved
 
     def update_team_calcs(self, teams: list) -> list:
@@ -163,8 +146,8 @@ class TBATeamCalc(base_calculations.BaseCalculations):
 
         tba_team_updates = {}
 
-        foul_ccs = self.calculate_foul_cc()
-        link_ccs = self.calculate_link_cc()
+        foul_ccs = self.calculate_cc(cc_type="foul")
+        link_ccs = self.calculate_cc(cc_type="link")
         for team in teams:
             # Load team data from database
             obj_tims = self.server.db.find("obj_tim", {"team_number": team})
